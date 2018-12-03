7c5dfff9b7b438eca74a8e2f4fb6e4e51dbd413b
James Kozlowski <jameskozlowski@gmail.com>
2018-12-02 22:00:56 -0400

    Added memory leak checks

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,4 +1,20 @@
+#ifdef _DEBUG
+#define _CRTDBG_MAP_ALLOC  
+#include <stdlib.h>  
+#include <crtdbg.h>  
+#endif
 
+#ifdef _DEBUG
+#define MYDEBUG_NEW   new( _NORMAL_BLOCK, __FILE__, __LINE__)
+// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+//allocations to be of _CLIENT_BLOCK type
+#else
+#define MYDEBUG_NEW
+#endif // _DEBUG
+
+#ifdef _DEBUG
+#define new MYDEBUG_NEW
+#endif
 
 #include <iostream>
 #include <stdexcept>
@@ -7,9 +23,15 @@
 
 #include "vulkanBase.h"
 
+
+
+
+
+
 int main() {
 	vulkanBase app;
 
+	//char* b = (char*)malloc(sizeof(char[10]));
 	try {
 		app.run();
 	}
@@ -17,6 +39,11 @@ int main() {
 		std::cerr << e.what() << std::endl;
 		return EXIT_FAILURE;
 	}
+
+#ifdef _DEBUG
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+	_CrtDumpMemoryLeaks();  
+#endif
 
 	return EXIT_SUCCESS;
 }
